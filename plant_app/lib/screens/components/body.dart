7e7d701030d115a62bb6acef4ce47b2b3013905c
Salman Khan <salmantechhub336@gmail.com>
2026-04-14 19:54:34 +0500

    appbar search done

---
+++ plant_app/lib/screens/components/body.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plant_app/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          height: size.height * 0.2,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                   padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor,
+                    // Opacity(0.23),
+                  ),
+                  ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hint: Text('Search'),
+                            hintStyle: TextStyle(
+                              color:kPrimaryColor,
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            // suffixIcon: SvgPicture.asset('assets/icons/search.svg'),
+                          ),
+                        ),
+                      ),
+                      SvgPicture.asset('assets/icons/search.svg'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ plant_app/lib/screens/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: kPrimaryColor,
+      leading: IconButton(
+        onPressed: (){
+          
+        }, 
+        icon: SvgPicture.asset('assets/icons/menu.svg')),
+    );
+  }
+}
