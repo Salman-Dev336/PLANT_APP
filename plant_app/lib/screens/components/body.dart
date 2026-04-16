99661e922130c6e07ff932ff315a552e9b6a4716
Salman Khan <salmantechhub336@gmail.com>
2026-04-16 19:39:17 +0500

    changes

--- plant_app/lib/screens/components/body.dart
+++ plant_app/lib/screens/components/body.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/components/header_with_search_box.dart';
+import 'package:plant_app/screens/components/recommend_plants.dart';
 import 'package:plant_app/screens/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -16,128 +17,11 @@ class Body extends StatelessWidget {
         children: [
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: 'Recommended', press: () {}),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                RecommendPlantCard(
-                  image: 'assets/images/image_1.png',
-                  title: 'Samantha',
-                  country: 'Russia',
-                  price: 500,
-                  press: () {},
-                  // size: size
-                ),
-                RecommendPlantCard(
-                  image: 'assets/images/image_2.png',
-                  title: 'Angelica',
-                  country: 'Pakistan',
-                  price: 500,
-                  press: () {},
-                  // size: size
-                ),
-                RecommendPlantCard(
-                  image: 'assets/images/image_3.png',
-                  title: 'Rose',
-                  country: 'Afghanistan',
-                  price: 500,
-                  press: () {},
-                  // size: size
-                ),
-              ],
-            ),
-          ),
+          RecommendsPlants(),
         ],
       ),
     );
   }
 }
 
-class RecommendPlantCard extends StatelessWidget {
-  const RecommendPlantCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.country,
-    required this.price,
-    required this.press,
 
-    // required this.size,
-  });
-
-  final String image, title, country;
-  final int price;
-  final Function press;
-
-  // final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      width: size.width * 0.35,
-      child: Column(
-        children: [
-          Image.asset(image),
-          GestureDetector(
-            onTap: press(),
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.35),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n",
-                          style: Theme.of(context).textTheme.labelLarge!
-                              .copyWith(
-                                // color: kPrimaryColor,
-                                fontWeight: FontWeight.bold,
-                              ),
-                        ),
-                        TextSpan(
-                          text: country,
-                          style: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.9),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    "\$$price",
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
