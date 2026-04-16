6069912be2f773e20a53177727f2ca3d17337115
Salman Khan <salmantechhub336@gmail.com>
2026-04-16 20:00:44 +0500

    changes

--- plant_app/lib/screens/components/body.dart
+++ plant_app/lib/screens/components/body.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/components/featured_plants.dart';
 import 'package:plant_app/screens/components/header_with_search_box.dart';
 import 'package:plant_app/screens/components/recommend_plants.dart';
 import 'package:plant_app/screens/components/title_with_more_btn.dart';
@@ -19,49 +20,14 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(title: 'Recommended', press: () {}),
           RecommendsPlants(),
           TitleWithMoreBtn(title: 'Featured Plants', press: () {}),
-          FeaturedPlantCard(
-            image: 'assets/images/bottom_img_1.png',
-            press: () {},
-            // size: size
-          )
+          FeaturedPlants(),
+          SizedBox(
+            height: kDefaultPadding,
+          ),
         ],
       ),
     );
   }
 }
 
-class FeaturedPlantCard extends StatelessWidget {
-  const FeaturedPlantCard({
-    super.key, required this.image, required this.press,
-    // required this.size,
-  });
-  final String image;
-  final Function press;
 
-  // final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: (){
-        press();
-      },
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding/2,
-          bottom: kDefaultPadding/2,
-        ),
-        width: size.width*0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            image: AssetImage(image),),
-          
-        ),
-      ),
-    );
-  }
-}
