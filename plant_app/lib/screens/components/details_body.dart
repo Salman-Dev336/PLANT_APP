01a98b8134b29af04878b4afc1f4d90fd5966537
Salman Khan <salmantechhub336@gmail.com>
2026-04-18 01:22:46 +0500

    changes

--- plant_app/lib/screens/components/details_body.dart
+++ plant_app/lib/screens/components/details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/constants.dart';
 
 class DetailsBody extends StatelessWidget {
@@ -13,7 +14,12 @@ class DetailsBody extends StatelessWidget {
           children: [
             Expanded(child: Column(
               children: [
-                
+                IconButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                 icon: SvgPicture.asset('assets/icons/back_arrow.svg'),),
+
               ],
             )),
             Container(
