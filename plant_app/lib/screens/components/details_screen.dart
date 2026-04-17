77ee14bd9f49a5d77e9cad851b0ac1f9ad764b47
Salman Khan <salmantechhub336@gmail.com>
2026-04-17 22:30:51 +0500

    changes

--- plant_app/lib/screens/components/details_screen.dart
+++ plant_app/lib/screens/components/details_screen.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: unused_import
-
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/components/body.dart';
 import 'package:plant_app/screens/components/details_body.dart';
+// import 'package:plant_app/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
