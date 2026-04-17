578af7b3c05200dff62c9e279cee50443c3cf070
Salman Khan <salmantechhub336@gmail.com>
2026-04-18 01:27:42 +0500

    changes

--- plant_app/lib/screens/components/details_body.dart
+++ plant_app/lib/screens/components/details_body.dart
@@ -10,42 +10,58 @@ class DetailsBody extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(
-          children: [
-            Expanded(child: Column(
-              children: [
-                IconButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                 icon: SvgPicture.asset('assets/icons/back_arrow.svg'),),
-
-              ],
-            )),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.29),
-                  )
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/img.png'),
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3,
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.29),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/img.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
